1cdff10f097e76c4e8f6b20938d53b374acffbe7
Michael Sanders <neoanomally@gmail.com>
2023-12-02 13:56:14 -0500

    Mes/day2 2023 (#13)
    * First day of advent of code 2023 done with tests
    
    * Modeled out the different components in day 2. Created independnt tests for each part. Solved part 1 and part 2

---
+++ src/main/scala/com/sandersme/advent/twentythree/CubeConundrum.scala
@@ -0,0 +1,30 @@
+package com.sandersme.advent.twentythree
+
+import com.sandersme.advent.Input
+import com.sandersme.advent.twentythree.model.{BagCubeContents, Cube, CubeColor}
+
+object CubeConundrum {
+
+  def main(args: Array[String]): Unit = {
+    val input = Input.readTwentyThreeFromResource("day2_input")
+
+    val games = Cube.parseAllGames(input)
+
+    val bagCubeContents = BagCubeContents(Map(
+      CubeColor.red -> 12,
+      CubeColor.blue -> 14,
+      CubeColor.green -> 13
+    ))
+
+    val sumOfPossibleCubes = Cube.sumIdsOfPossibleGames(games, bagCubeContents)
+
+    println(s" The sum of all possible game cube IDs: ${sumOfPossibleCubes}")
+
+    val sumOfGamePowers = games
+      .map(_.powerOfGame)
+      .sum
+
+    println(s"The sum of the power of all games: ${sumOfGamePowers}")
+  }
+
+}
