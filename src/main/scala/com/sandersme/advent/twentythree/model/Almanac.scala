0dc29c4c0072c54f7fe5c0d580ee69cc65dfc417
Michael Sanders <neoanomally@gmail.com>
2023-12-08 22:52:07 -0500

    Finished part 2 for day 5

--- src/main/scala/com/sandersme/advent/twentythree/model/Almanac.scala
+++ src/main/scala/com/sandersme/advent/twentythree/model/Almanac.scala
@@ -1,7 +1,6 @@
 package com.sandersme.advent.twentythree.model
 
 import scala.annotation.tailrec
-import com.sandersme.advent.twentytwo.model.PacketType.startParsing
 
 /**
   * This is a container for the input seeds and a collection of all the maps @see AlmondMap.
@@ -45,6 +44,35 @@ case class Almanac(seeds: Seeds, almanacMaps: List[AlmanacMap]) {
     loop(startingPoint)
   }
 
+/**
+  * This is method will go through the list of seed ranges and map them to the locations
+  * should return a list of translated seed ranges that are mapped to the final location
+  *
+  * @return
+  */  
+  def findSeedRangeLocations: List[SeedRange] = {
+    val startingMap: AlmanacMap = getAlmanacBySource(ENTRY)
+
+    @tailrec
+    def loop(almanacMap: AlmanacMap, seedRanges: List[SeedRange]): List[SeedRange] = {
+      val translatedValues = almanacMap.translateSeedRangeToDestination(seedRanges)
+      if (almanacMap.destination == TERMINATION) 
+        translatedValues
+      else
+        val nextMap = getAlmanacBySource(almanacMap.destination)
+        loop(nextMap, translatedValues)
+    }
+
+    // println(s"Created Seed Ranges: ${seeds.createSeedRanges}")
+    loop(startingMap, seeds.createSeedRanges)
+  }
+
+  def findSeedRangeMinLocation: Long = {
+    findSeedRangeLocations
+      .map(_.start)
+      .min
+  }
+
   /**
     *  This method hops from source to destination until it finds 'location' as it's
     *  source. Each step along the way it translates it's starting value using the
@@ -70,8 +98,33 @@ case class Almanac(seeds: Seeds, almanacMaps: List[AlmanacMap]) {
    def findLowestLocation: Long = {
     findSeedLocations.min
    }
+
+   def countTotalNumberSeeds: Long = {
+     seeds.createSeedRanges
+       .map(_.spanLength)
+       .sum
+   }
 }
 
+
+case class SeedRange(start: Long, stop: Long) {
+  def spanLength: Long = stop - start + 1
+}
+
+object SeedRange {
+  def fromSpanLength(start: Long, spanLength: Long): SeedRange = {
+    SeedRange(start, start + spanLength - 1)
+  }
+
+  def validateSeedRange(seedRange: SeedRange): Option[SeedRange] = {
+    if( seedRange.start > seedRange.stop)
+      None 
+    else if (seedRange.stop < seedRange.start)
+      None
+    else 
+      Some(seedRange)
+  }
+}
 
 case class AlmanacRange(destinationStart: Long, sourceStart: Long, rangeLength: Long) {
   /**
@@ -84,6 +137,61 @@ case class AlmanacRange(destinationStart: Long, sourceStart: Long, rangeLength: 
     */
   def inSourceRange(seed: Long): Boolean = {
     sourceStart <= seed && seed <= sourceMax
+  }
+  
+/**
+  * The following method translates a seedrange from source to destination. Warning there
+  * is no guarantee within this method that the translation will put you in the correct
+  * range due to being over the span. this is done in a different method.
+  *
+  * Ideally in a real world situation I would either error out or send the maximum. 
+  *
+  * @param seedRange
+  * @return
+  */
+  def destinationFromRange(seedRange: SeedRange): SeedRange = {
+    SeedRange(seedRange.start - difference, seedRange.stop - difference)
+  }
+
+  def splitSeedRangesTranslated(seedRange: SeedRange): (List[SeedRange], Option[SeedRange]) = {
+    val (remaining, seedInRange) = splitSeedRange(seedRange)
+    val translated = seedInRange.map(destinationFromRange)
+
+    (remaining, translated)
+  }
+
+  /**
+    * Get all the values below the mappingFromSegmentSlicing() 
+    *
+    * @param seedRange
+    * @return
+    */
+  def splitSeedRange(seedRange: SeedRange): (List[SeedRange], Option[SeedRange]) = {
+    // 79 97 with a Range => 98, 99
+    // get all values below   (seed.min - map.start - 1) 
+    // get all values aboe (map.sourceMax + 1 - seed.max) 
+    // get the intersection of all values Math.min stuff.
+    //
+    val allValuesBelow = SeedRange(seedRange.start, Math.min(seedRange.stop, sourceStart - 1))
+    val allValuesAbove = SeedRange(Math.max(sourceMax + 1, seedRange.start), seedRange.stop)
+
+    val intersectionStart = Math.max(sourceStart, seedRange.start)
+    val intersectionStop = Math.min(sourceMax, seedRange.stop)
+    val allValuesBetween = SeedRange(intersectionStart, intersectionStop)
+
+    val validatedBetween = SeedRange.validateSeedRange(allValuesBetween)
+    
+    val stillNeedProcessing = List(allValuesBelow, allValuesAbove)
+      .flatMap(SeedRange.validateSeedRange)
+
+    (stillNeedProcessing, validatedBetween)
+  }
+
+  def isSeedRangeInRange(seedRange: SeedRange): Boolean = {
+    // 0, 5 || 10, 20
+    val isStartBetween = seedRange.start >= sourceStart && seedRange.start <= sourceMax
+    val isStopBetween = seedRange.stop >= sourceStart && seedRange.stop <= sourceMax
+    isStartBetween || isStopBetween
   }
 
   /**
@@ -114,6 +222,15 @@ case class AlmanacRange(destinationStart: Long, sourceStart: Long, rangeLength: 
     }
   }
 }
+
+/**
+  * Case class that holds the source and desination identifier. Then it contains the list
+  * of AlmanacRanges
+  *
+  * @param source
+  * @param destination
+  * @param ranges
+  */
 case class AlmanacMap(source: String, destination: String, ranges: List[AlmanacRange]) {
   /**
     * Given a seed number map to the correct destination number. This is done by 
@@ -128,9 +245,48 @@ case class AlmanacMap(source: String, destination: String, ranges: List[AlmanacR
       .flatMap(_.destinationFromSource(seed))
       .getOrElse(seed)
   }
+
+  // This method goes over every Range. Then it processes the list of seed ranges not
+  // found. At every step we split out any non-overlapping seed ranges that fit within the
+  // range. We keep an accumulation of seeds that are remaining to be processed and the
+  // successfully translated ranges. Finally at the end we update to the final method. 
+  def translateSeedRangeToDestination(seedRanges: List[SeedRange]): List[SeedRange] = {
+    val defaultTranslatedRanges = List.empty[SeedRange]
+    val accumulator = (seedRanges, defaultTranslatedRanges)
+    val (nonTranslatedSeeds, translatedSeeds) = ranges
+      .foldLeft(accumulator){ case((seeds, translated), range) => 
+        val splitAndTranslated = seeds
+          .map(range.splitSeedRangesTranslated)
+        
+        val seedRangesRemaining = splitAndTranslated.flatMap(_._1)
+        val translatedSeedRanges = splitAndTranslated.flatMap(_._2)
+        val accumulated = translated ++ translatedSeedRanges
+
+        (seedRangesRemaining, accumulated)
+      }
+
+    nonTranslatedSeeds ++ translatedSeeds
+  }
 }
 
-case class Seeds(values: List[Long])
+case class Seeds(values: List[Long]) {
+   def createSeedRanges: List[SeedRange] = {
+      @tailrec
+      def loop(remaining: List[Long], pairs: List[SeedRange]): List[SeedRange] = {
+        if (remaining.isEmpty)
+          pairs
+        else {
+          val left = remaining.head
+          val right = remaining.tail.head
+          val updatedPairs = pairs :+ SeedRange.fromSpanLength(left, right)
+
+          loop(remaining.drop(2), updatedPairs)
+        }
+      }
+
+      loop(values, List.empty)
+   }
+}
 
 
 object Almanac {
@@ -184,7 +340,8 @@ object Almanac {
     * is going to fold Left accumulating a SeedMap. 
     *
     * @param input
-    * @return
+    * @return The result is a list of the AlmanacMaps containing the lists of ranges as
+    * well as the source and destination in a case class. 
     */
   def parseAlmanacMaps(input: List[String]): List[AlmanacMap] = {
     @tailrec
--- src/test/scala/com/sandersme/advent/twentythree/model/AlmanacTest.scala
+++ src/test/scala/com/sandersme/advent/twentythree/model/AlmanacTest.scala
@@ -1,9 +1,104 @@
 package com.sandersme.advent.twentythree.model
 
+import com.sandersme.advent.twentythree.model.Almanac.parseAlmanacMaps
+
 class AlmanacTest extends munit.FunSuite {
   test("This is a temporary Test to print out the traversal") {
     val almanac = Almanac.parseInput(TEST_INPUT)
+    // println(almanac.countTotalNumberSeeds)
     //    almanac.printMapTraversal
+  }
+
+  test("Test the seed ranges map to the correct values") {
+    val seedRangeLeft = SeedRange(79, 92)
+    val seedRangeRight = SeedRange(55, 67)
+    val bothSeedRanges = List(seedRangeLeft, seedRangeRight)
+
+    val maps = Almanac.parseInput(TEST_INPUT)
+    val firstMap = maps.almanacMaps.head
+
+    val leftTranslated = firstMap.ranges.head.splitSeedRangesTranslated(seedRangeLeft)
+    val rightTranslated = firstMap.ranges.head.splitSeedRangesTranslated(seedRangeRight)
+    val translated = firstMap.translateSeedRangeToDestination(bothSeedRanges)
+   
+    val expectedLeftTranslated = (List(SeedRange(79, 92)), None)
+    val expectedRightTranslated = (List(SeedRange(55, 67)), None)
+    
+    assertEquals(leftTranslated, expectedLeftTranslated)
+    assertEquals(rightTranslated, expectedRightTranslated)
+  }
+
+  test("Find the final seed range locations using test data") {
+    val almanac = Almanac.parseInput(TEST_INPUT)
+
+    val seedRangeLocations = almanac.findSeedRangeLocations 
+    val expectedMin = 46L
+    val min = almanac.findSeedRangeMinLocation
+    assertEquals(min, expectedMin)
+  }
+
+
+  // TODO if we split up too many ranges we may actually need to merge them at the end. 
+  test("translateSeedRangeToDestination goes over the set of seed translation for a single Map") {
+    val almanac = Almanac.parseInput(TEST_INPUT)
+    val translatedSeeds = almanac.almanacMaps
+      .head.translateSeedRangeToDestination(almanac.seeds.createSeedRanges)
+    // TODO Write a real test
+  }
+
+  test("Take an almanacRange and map the destination") {
+    val almanacRange = AlmanacRange(0, 10, 10)
+    val seedRange = SeedRange(13, 20)
+
+    val expectedSeedRange = SeedRange(3, 10)
+    val translatedSeedRange = almanacRange.destinationFromRange(seedRange)
+    assertEquals(translatedSeedRange, expectedSeedRange)
+  }
+
+  test("Take an almanacRange and split seedrange into remaining Ranges and Processed Ranges") {
+    val almanacRange = AlmanacRange(0, 10, 10)
+    val seedRange = SeedRange(0, 30) // should have 0 - 9, 21 - 30: 10-20
+
+    val (remaining, processed) = almanacRange.splitSeedRange(seedRange)
+
+    val expectedRemaining = List(SeedRange(0, 9), SeedRange(20, 30))
+    val expectedProcessed = Option(SeedRange(10, 19))
+
+    assertEquals(remaining, expectedRemaining)
+    assertEquals(processed, expectedProcessed)
+  }
+
+  test("Validate that seed ranges are valid ranges e.g. start is < stop and vicee versa") {
+    val seedRangeValid  = SeedRange.validateSeedRange(SeedRange(5, 10))
+    val seedRangeValidAtPoint = SeedRange.validateSeedRange(SeedRange(5,5))
+    val seedRangeInvalidA =  SeedRange.validateSeedRange(SeedRange(21, 5))
+    val seedRangeInvalidB =  SeedRange.validateSeedRange(SeedRange(5, -1))
+
+    assertEquals(seedRangeValid, Some(SeedRange(5, 10)))
+    assertEquals(seedRangeInvalidB, None)
+    assertEquals(seedRangeValidAtPoint, Some(SeedRange(5, 5)))
+    assertEquals(seedRangeInvalidB, None)
+  }
+
+  // TODO FInd a funciton that turns SeedRange into three chunks:
+  // 1. What's overlapping (Math.min(range.start, seed.stop)) (Math.max(range.stop,
+  //    seed.start))
+  // 2. What's left below (e.g. seed.stop < range.start)
+  // 3. What's left above (e.g. seed.start > range.stop
+  // Combine steps 2 & 3 into remainingRanges. and then put 1 into the new range.
+
+  test("Check if seed range is in AlchemyRange returns the correct values.") {
+    val almanacRange = AlmanacRange(0, 10, 11) // this means the values are 10-20 
+    val seedRangeAIsInRange = SeedRange(5, 15) // this means values are 5 - 15
+    val seedRangeBIsInRange = SeedRange(15, 25) // this means the values are 15 - 25
+    val seedRangeCIsNotInRange = SeedRange(0, 5) // below values
+    val seedRangeDIsNotInRange = SeedRange(21, 26) // above values
+
+
+    assertEquals(almanacRange.isSeedRangeInRange(seedRangeAIsInRange), true)
+    assertEquals(almanacRange.isSeedRangeInRange(seedRangeBIsInRange), true)
+    assertEquals(almanacRange.isSeedRangeInRange(seedRangeCIsNotInRange), false)
+    assertEquals(almanacRange.isSeedRangeInRange(seedRangeDIsNotInRange), false)
   }
 
   test("Find that the lowest seed location from TEST input is 35") {
@@ -119,6 +214,15 @@ class AlmanacTest extends munit.FunSuite {
     assertEquals(almanacMaps.last, expectedTail)
   }
 
+
+  test("Create various Seed range pairs from the input") {
+    val seeds = Seeds(List(79L, 14L, 55L, 13L))
+
+    val expectedSeedRanges = List(SeedRange(79L, 92L), SeedRange(55L, 67L))
+    val seedRanges = seeds.createSeedRanges
+
+    assertEquals(seedRanges, expectedSeedRanges)
+  }
 
   val TEST_INPUT: List[String] = """seeds: 79 14 55 13
 
