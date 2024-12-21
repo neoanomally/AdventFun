a215d2b2a55384e220a31f8619058bda284da913
Michael Sanders <neoanomally@gmail.com>
2024-12-21 17:49:06 -0500

    Checkpoint for part 1. Spent time building out the logic to create all possible paths

--- src/main/scala/com/sandersme/advent/twentyfour/KeypadConundrum.scala
+++ src/main/scala/com/sandersme/advent/twentyfour/KeypadConundrum.scala
@@ -3,14 +3,21 @@ package com.sandersme.advent.twentyfour
 import com.sandersme.advent.twentyone.model.Cuboid.intersection
 
 
-case class KeypadConundrum(keypadGraph: Map[Char, Point], directionGraph: Map[Char, Point], instructions: List[List[Char]])
+case class KeyGraph(valueLookup: Map[Char, Point], pointLookup: Map[Point, Char], height: Int, width: Int)
+
+case class KeypadConundrum(keypadGraph: KeyGraph, directionGraph: KeyGraph, instructions: List[List[Char]]) {
+
+}
+case class KeyPath(value: Char, direction: Char, path: List[Char]) {
+  
+}
 
 object KeypadConundrum {
   def main(args: Array[String]): Unit = {
     println("hi")
   }
 
-  def createKeyPoints(graph: Vector[Vector[Char]]): Map[Char, Point] = {
+  def createKeyPoints(graph: Vector[Vector[Char]]): KeyGraph = {
     val graphPosSeq = for { 
       y <- 0 until graph.length
       x <- 0 until graph(0).length
@@ -18,14 +25,59 @@ object KeypadConundrum {
       if (graph(y)(x) != '-')
     } yield graph(y)(x) -> Point(x, y)
 
-    graphPosSeq.toMap
+    KeyGraph(graphPosSeq.toMap, graphPosSeq.map(_.swap).toMap, graph.length, graph(0).length)
   }
 
 
-  def calculateShortestPath(): Map[Int, List[Char]] = {
-    ???
+  def calculateShortestPaths(graph: KeyGraph): Map[Char, Map[Char,  List[List[Char]]]] = {
+    graph.valueLookup
+      .foldLeft(Map.empty[Char, Map[Char, List[List[Char]]]]){ case (res, (value, _)) =>
+        
+        val keyPath = KeyPath(value, '-', List.empty)
+
+        val shortestPaths = calculateShortestPath(List(keyPath), graph, Map.empty)
+        
+        res + (value -> shortestPaths) 
+      }
   }
 
+  /// For the keypath it might be better just rto return the value
+  // TODO I should probably have both the value that it goes over as well 
+  // The different direction path; however the direction path can be used
+  // to calculate across ROBOTs
+  def calculateShortestPath(queue: List[KeyPath], keyGraph: KeyGraph,
+    res: Map[Char, List[List[Char]]]): Map[Char, List[List[Char]]] = {
+
+    if (queue.isEmpty) {
+      res
+    } else {
+      val updatedQueue = queue.flatMap{ keyPath =>
+        findGridNeighbors(keyPath, keyGraph)
+          .filter(neighborKeypath => !res.contains(neighborKeypath.value))
+      }
+
+      val updatedMap = updatedQueue.groupBy(keyPath => keyPath.value)
+        .map((k, v) => k -> v.map(_.path))
+      
+      val updatedRes = res ++ updatedMap
+      calculateShortestPath(updatedQueue, keyGraph, updatedRes)
+    }
+  }
+
+  /// TODO Instead of points for the keypath lookup might be better just to return the
+  //value
+  def findGridNeighbors(keyPath: KeyPath, keyGraph: KeyGraph): List[KeyPath] = {
+    val point = keyGraph.valueLookup(keyPath.value)
+    val updatedPath = keyPath.path :+ keyPath.direction
+    List(
+      (Point(point.x + 1,point.y), '>'),
+      (Point(point.x - 1, point.y), '<'),
+      (Point(point.x, point.y + 1), 'v'),
+      (Point(point.x, point.y - 1), '^')
+    ).filter((p, _) => p.x >= 0 && p.y >= 0 && p.x < keyGraph.width && p.y < keyGraph.height)
+      .filter((p, _)  => keyGraph.pointLookup.contains(p))
+      .map((point, dir) => KeyPath(keyGraph.pointLookup(point), dir, updatedPath))
+  }
 
   def parseInput(in: List[String]): KeypadConundrum = {
     val instructions = in.map(_.toCharArray.toList)
--- src/test/scala/com/sandersme/advent/twentyfour/KeypadConundrumTest.scala
+++ src/test/scala/com/sandersme/advent/twentyfour/KeypadConundrumTest.scala
@@ -1,7 +1,46 @@
 package com.sandersme.advent.twentyfour
 
 class KeypadConundrumTest extends munit.FunSuite {
-  test("CHILDREN") {
+  test("Test creating the graph and verifying points are correct") {
+    val testGraph = KeypadConundrum.createKeyPoints(NumericKeypadGraph)
 
+    assertEquals(testGraph.valueLookup('7'), Point(0, 0))
+    assertEquals(testGraph.valueLookup('0'), Point(1, 3))
   }
+
+
+  test("Test taht we can create a path fora ny given point to all other points") {
+    val testGraph = KeypadConundrum.createKeyPoints(NumericKeypadGraph)
+    val point = Point(0, 0)
+    
+    val shortestPath = KeypadConundrum.calculateShortestPath(List(KeyPath('7', '-', List.empty)), testGraph, Map.empty )
+
+    val eightShortest = shortestPath('8') 
+    val expected = List(List('-'))
+
+    assertEquals(eightShortest, expected)
+  }
+
+  test("Create shortest path with the directional input") {
+    val testGraph = KeypadConundrum.createKeyPoints(DirectionalKeyPad)
+
+    val point = Point(1, 0)
+
+    val shortestPath = KeypadConundrum.calculateShortestPath(List(KeyPath('^', '-', List.empty)), testGraph, Map.empty)
+    val leftArrowShortest = shortestPath('<')
+    val expected = List(List('-', 'v')) 
+
+    assertEquals(leftArrowShortest, expected)
+  }
+
+  val TEST_INPUT = """029A
+                      980A
+                      179A
+                      456A
+                      379A""".stripMargin
+                      .split("\n")
+                      .toList
+                      .map(_.trim)
+
+  val KEYBOARD_CONUNDRUM = KeypadConundrum.parseInput(TEST_INPUT)
 }
