a06ef45bebd5dd3b4cac4e84cb6973be2ad7f5f6
Michael Sanders <neoanomally@gmail.com>
2024-12-23 12:28:26 -0500

    Finished part 1 and 2 of Keypad Conundrum day 21

--- src/main/scala/com/sandersme/advent/twentyfour/KeypadConundrum.scala
+++ src/main/scala/com/sandersme/advent/twentyfour/KeypadConundrum.scala
@@ -1,20 +1,156 @@
 package com.sandersme.advent.twentyfour
 
-import com.sandersme.advent.twentyone.model.Cuboid.intersection
+import com.sandersme.advent.twentyfour.PathGraph
+import com.sandersme.advent.Input
+import scala.collection.mutable.{Map => MMap}
+import scala.annotation.tailrec
 
 
-case class KeyGraph(valueLookup: Map[Char, Point], pointLookup: Map[Point, Char], height: Int, width: Int)
+type PathGraph = Map[Char, Map[Char, List[List[Char]]]]
 
-case class KeypadConundrum(keypadGraph: KeyGraph, directionGraph: KeyGraph, instructions: List[List[Char]]) {
-
-}
-case class KeyPath(value: Char, direction: Char, path: List[Char]) {
+case class KeyGraph(valueLookup: Map[Char, Point], pointLookup: Map[Point, Char], height: Int, width: Int) {
   
 }
 
+case class KeypadConundrum(keypadGraph: KeyGraph, directionGraph: KeyGraph, keyPaths: PathGraph, directionPaths: PathGraph, instructions: List[String], numRobots: Int) { 
+  def calculateShortestPathCosts: Long = {
+    instructions
+      .map(inst => inst.dropRight(1).toLong * calculateShortestPath(inst))
+      .sum
+  }
+
+  def calculateShortestPath(combination: String): Int = {
+    calculateSecondPath(combination)
+      .flatMap(in => calculatePath(in, directionPaths, MMap.empty))
+      .map(_.size)
+      .min
+  }
+
+  def calculateSecondPath(combination: String): List[List[Char]] = {
+    calculateFirstPath(combination)
+      .flatMap(in =>  calculatePath(in, directionPaths, MMap.empty))
+  }
+
+
+
+  def calculateFirstPath(combination: String): List[List[Char]] = {
+    calculatePath(combination.toList, keyPaths, MMap.empty)
+  }
+
+  def calculateCostPartTwo: Long = {
+    
+    instructions
+      .map{ combination => 
+        val cache = MMap.empty[(Char, Char, Int), Long]
+        val cost = calculatePathPartTwo(combination.toList, cache, 0)
+        val multiple = combination.dropRight(1).toLong
+        cost * multiple
+      }
+      .sum
+  }
+
+  // DO I NEED TO DO SOMETHING SPECIFIC FOR THE 25th value?
+  def calculatePathPartTwo(combination: List[Char], cache: MMap[(Char, Char, Int), Long], stage: Int): Long = {
+    val keypad = if (stage > 0) directionPaths else keyPaths
+
+    combination.foldLeft(('A', 0L)){ case ((start, sum), end) =>
+      val shortestDist = cache.getOrElseUpdate((start, end, stage), {
+        shortestCurrentPath(start, end, stage, cache)
+      })
+
+      (end, sum +  shortestDist)
+    }._2
+  }
+
+  /// So I should start with the first value so in the case of 029A
+  // We'll first hit 0, and then for that we'll find the shortest path for the first robot
+  // to hit zero.
+  // Once there the next robot will find the shortest path to get the robot to zero from
+  // A
+  // The next robot will take the list of sequences starting from A -> next value
+  def shortestCurrentPath(start: Char, end: Char, stage: Int, cache: MMap[(Char, Char, Int), Long]): Long = {
+    val graph = if (stage == 0) { keyPaths } else { directionPaths }
+    
+    if (start == end) {
+      1L
+    } else {
+
+    graph(start)(end).map{ case combination => 
+        if (stage < numRobots) {
+          val dist = calculatePathPartTwo(combination :+ 'A', cache, stage + 1)
+          dist
+        } else {
+          combination.size.toLong + 1L // The 1L is to acccount for clicking A
+        }
+      }.min
+    }
+  }
+
+  def findShortestCombinations(start: Char, end: Char, stage: Int): List[List[Char]] = {
+    val keypad = if (stage > 0) directionPaths else keyPaths
+
+    // println("CALCULATING SHORTEST COMBO STAGE: " + stage + f" START: $start  END: $end ")
+    keypad(start)(end).map(_ :+ 'A')
+  }
+
+  // Knowing this will help calculate the second path. 
+  def calculatePath(combination: List[Char], graph: PathGraph, cache: MMap[(Char, Char, Int), Long]): List[List[Char]] = {
+    val (lastLoc, shortestPath) = combination.foldLeft(('A', List.empty[List[Char]])) { case ((startLoc, sum), nextLoc) => 
+
+      val distances = if (startLoc == nextLoc) {
+        List(List('A'))
+      } else {
+        val graphDist = graph(startLoc)(nextLoc).map(_ :+ 'A')
+        val minSizes = graphDist.map(_.size).min
+        graphDist.filter(_.size == minSizes)
+      }
+
+      val f = if (sum.isEmpty) {
+        distances
+      } else { 
+        sum.flatMap(sofar => distances.map(dist => sofar ++ dist))
+      }
+      (nextLoc, f)
+    }
+
+    shortestPath
+  }
+}
+
+
+case class KeyPath(value: Char, direction: Char, path: List[Char]) {
+  def createEnd: KeyPath = {
+    // maybe this should be char
+    this.copy(path = path.tail :+ direction)
+  }
+}
+
+
+/**
+  * One directional keypad that you are using.
+    Two directional keypads that robots are using.
+    One numeric keypad (on a door) that a robot is using.
+
+    WE can see the path for just the numeric keypad that the final 
+    robot should do. Make sure that path works
+  
+    Then go up etc. 
+  */
 object KeypadConundrum {
   def main(args: Array[String]): Unit = {
-    println("hi")
+    val input = Input.readTwentyFourFromResource("day21_input")
+
+    val keypadConundrum = KeypadConundrum.parseInput(input)
+    // val shortestPathCost = keypadConundrum.calculateShortestPathCosts
+    //
+    // println(f"The cost for the shortest path is: $shortestPathCost")
+    val shortestPathCostPartOne = keypadConundrum.copy(numRobots = 2).calculateCostPartTwo
+
+    println(f"The cost for the shortest path in part one is $shortestPathCostPartOne and should be 179444")
+
+    val partTwoCost = keypadConundrum.calculateCostPartTwo
+    println(keypadConundrum.numRobots)
+    println(f"The cost for shortest Path for 25 robots is: $partTwoCost")
   }
 
   def createKeyPoints(graph: Vector[Vector[Char]]): KeyGraph = {
@@ -29,7 +165,7 @@ object KeypadConundrum {
   }
 
 
-  def calculateShortestPaths(graph: KeyGraph): Map[Char, Map[Char,  List[List[Char]]]] = {
+  def calculateShortestPaths(graph: KeyGraph): Map[Char, Map[Char, List[List[Char]]]]= {
     graph.valueLookup
       .foldLeft(Map.empty[Char, Map[Char, List[List[Char]]]]){ case (res, (value, _)) =>
         
@@ -40,6 +176,7 @@ object KeypadConundrum {
         res + (value -> shortestPaths) 
       }
   }
+
 
   /// For the keypath it might be better just rto return the value
   // TODO I should probably have both the value that it goes over as well 
@@ -56,13 +193,15 @@ object KeypadConundrum {
           .filter(neighborKeypath => !res.contains(neighborKeypath.value))
       }
 
-      val updatedMap = updatedQueue.groupBy(keyPath => keyPath.value)
+      val updatedMap = updatedQueue.map(keyPath => keyPath.copy(path = keyPath.path.tail :+ keyPath.direction))
+        .groupBy(keyPath => keyPath.value)
         .map((k, v) => k -> v.map(_.path))
       
       val updatedRes = res ++ updatedMap
       calculateShortestPath(updatedQueue, keyGraph, updatedRes)
     }
   }
+
 
   /// TODO Instead of points for the keypath lookup might be better just to return the
   //value
@@ -80,12 +219,15 @@ object KeypadConundrum {
   }
 
   def parseInput(in: List[String]): KeypadConundrum = {
-    val instructions = in.map(_.toCharArray.toList)
+    val instructions = in
 
     val numericpad = createKeyPoints(NumericKeypadGraph)
     val directionalpad = createKeyPoints(DirectionalKeyPad)
+  
+    val numericPaths = calculateShortestPaths(numericpad)
+    val directionPaths = calculateShortestPaths(directionalpad)
 
-    KeypadConundrum(numericpad, directionalpad, instructions)
+    KeypadConundrum(numericpad, directionalpad, numericPaths, directionPaths, instructions, 25)
   }
 }
 
--- src/test/scala/com/sandersme/advent/twentyfour/KeypadConundrumTest.scala
+++ src/test/scala/com/sandersme/advent/twentyfour/KeypadConundrumTest.scala
@@ -1,5 +1,7 @@
 package com.sandersme.advent.twentyfour
 
+import scala.collection.mutable.{Map => MMap }
+
 class KeypadConundrumTest extends munit.FunSuite {
   test("Test creating the graph and verifying points are correct") {
     val testGraph = KeypadConundrum.createKeyPoints(NumericKeypadGraph)
@@ -15,8 +17,8 @@ class KeypadConundrumTest extends munit.FunSuite {
     
     val shortestPath = KeypadConundrum.calculateShortestPath(List(KeyPath('7', '-', List.empty)), testGraph, Map.empty )
 
-    val eightShortest = shortestPath('8') 
-    val expected = List(List('-'))
+    val eightShortest = shortestPath('8')
+    val expected = List(List('>'))
 
     assertEquals(eightShortest, expected)
   }
@@ -28,11 +30,61 @@ class KeypadConundrumTest extends munit.FunSuite {
 
     val shortestPath = KeypadConundrum.calculateShortestPath(List(KeyPath('^', '-', List.empty)), testGraph, Map.empty)
     val leftArrowShortest = shortestPath('<')
-    val expected = List(List('-', 'v')) 
+    val expected = List(List('v', '<')) 
 
     assertEquals(leftArrowShortest, expected)
   }
 
+  test("Test the length of a path between two points on keymap") {
+    val shortestPath = KEYBOARD_CONUNDRUM.calculateFirstPath("029A")
+    val expected = List('<', 'A', '^', 'A', '>', '^', '^', 'A', 'v', 'v', 'v', 'A')
+
+    assertEquals(shortestPath.head, expected)
+  }
+
+  test("Calculate ShortestPath for second robot") {
+    val input = List('<', 'A', '^', 'A', '>', '^', '^', 'A', 'v', 'v', 'v', 'A')
+    val shortestPath = KEYBOARD_CONUNDRUM.calculatePath(input, KEYBOARD_CONUNDRUM.directionPaths, MMap.empty)
+
+    val shouldExist = shortestPath.exists( c => c.mkString == "v<<A>>^A<A>AvA<^AA>A<vAAA>^A")
+    assertEquals(shouldExist, true)
+  }
+
+  /// TODO: UNIGNORE ONCE I FIND THE cahce method
+  test("TESTING MYH SANITY this will be ignored due to how much it costs".ignore) {
+    val shortest = KEYBOARD_CONUNDRUM.calculateShortestPathCosts
+
+    assertEquals(shortest, 126384L)
+  }
+
+  test("TESTING MY SANITY PART TWO WILL BE IGNORED") {
+    val shortest = KEYBOARD_CONUNDRUM.copy(numRobots = 2, instructions = List("029A"))
+      .calculateCostPartTwo
+
+    val expectedResult = 68 * 29L
+
+    assertEquals(shortest, expectedResult)
+  }
+  test("TESTING MY SANITY PART TWO PART TWO PART TWO ") {
+    val shortest = KEYBOARD_CONUNDRUM.copy(numRobots = 2, instructions = List("980A"))
+      .calculateCostPartTwo
+
+    val expectedResult = 60L * 980L
+
+    assertEquals(shortest, expectedResult)
+  }
+
+  test("Test all combinations for apart one using the second solution".ignore) {
+    val shortest = KEYBOARD_CONUNDRUM.copy(numRobots = 2)
+      .calculateCostPartTwo
+
+    println("SHortest: " + shortest)
+  }
+
+// <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
+// v<<A   >>^A     <A >A vA <^A A >A <vA A A>^A
+// <A     ^A      >^^A vvvA
+// 
   val TEST_INPUT = """029A
                       980A
                       179A
