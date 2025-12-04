3fdd5487a59b69ae3997cb9b087f68fd4995d4f1
Michael Sanders <neoanomally@gmail.com>
2025-12-04 18:51:04 -0500

    Rust setup and day 1 2025

--- rust-AdventFun/src/main.rs
+++ rust-AdventFun/src/main.rs
@@ -1,9 +1,16 @@
-fn main() {
-    println!("Hello, world!");
-    let f = String::from("hello");
+mod advent_io;
+mod advent_2025;
 
+fn main() { 
+    
+    // let config = advent_file::read_default_config();
+    
+    // for (k, v) in config {
+    //     println!("{}  --  {}", k, v);
+    // }
 
-    f.to_uppercase();
+    advent_2025::day1::run_day_one_part_one();
+    // advent_2025::day1::rotate_dial_test_data();
 
 
 }
